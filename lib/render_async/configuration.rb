088e74eb7888cd41ee58995b0fd9d06f8ef29be1
Joe Milsom <joe.milsom@elmatica.com>
2020-05-26 14:07:39 +0200

    add option to use rails ajax
    why: when we are polling we want to have javascript to be executed in the response. rails ajax makes it so we can execute it without a CSP error

--- lib/render_async/configuration.rb
+++ lib/render_async/configuration.rb
@@ -1,10 +1,11 @@
 module RenderAsync
   class Configuration
-    attr_accessor :jquery, :turbolinks
+    attr_accessor :jquery, :turbolinks, :rails_ajax
 
     def initialize
       @jquery = false
       @turbolinks = false
+      @rails_ajax = false
     end
   end
 end
